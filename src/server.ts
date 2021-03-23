8d388440224e7e1180f3a632820dbf7f418301e8
augustorl <gutin22@gmail.com>
2021-03-22 22:12:02 -0300

    Update: Server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/specifications", specificationsRoutes);
 app.use("/categories", categoriesRoutes);
+
 app.listen(3333, () => console.log("Server runing on Port:3333"));
