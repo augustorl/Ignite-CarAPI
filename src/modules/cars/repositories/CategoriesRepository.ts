5b4e8696fea65d99a2104b7abbaeeeb741851c7a
augustorl <gutin22@gmail.com>
2021-03-22 23:01:09 -0300

    Update: Create INSTANCE

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,10 +7,19 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
   }
 
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
+  }
   findByName(name: string): Category {
     const category = this.categories.find((category) => category.name === name);
 
