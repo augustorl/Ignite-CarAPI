5f089464dcdb1fab60097f480e0bb588dd72bb44
augustorl <gutin22@gmail.com>
2021-03-30 19:32:39 -0300

    add: dependency injection to specifications

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,12 @@
+import { Specification } from "../entities/Specification";
+
+interface ICreateSpecificationsDTO {
+  name: string;
+  description: string;
+}
+interface ISpecificationsRepository {
+  create({ description, name }: ICreateSpecificationsDTO): Promise<void>;
+  findByName(name: string): Promise<Specification>;
+}
+
+export { ISpecificationsRepository, ICreateSpecificationsDTO };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,15 +2,15 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
-import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
 );
 
-container.registerSingleton<ISpecificationRepository>(
-  "SpecificationRepository",
-  SpecificationRepository
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
 );
