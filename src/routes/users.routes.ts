a1cbfd44715d3f751d0189aa3a17c672b8a9b002
augustorl <gutin22@gmail.com>
2021-03-31 02:33:22 -0300

    Fix: User

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureUserIsAuthenticated } from "../middlewares/ensureUserIsAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
-
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
+usersRoutes.patch(
+  "/avatar",
+  ensureUserIsAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 export { usersRoutes };
