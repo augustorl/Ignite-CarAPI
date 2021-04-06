663e1584a076451de47fd45345bf57b1468ad6a3
augustorl <gutin22@gmail.com>
2021-04-06 00:57:24 -0300

    update: CreateCarUseCase and test

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -2,9 +2,8 @@ import { ICreateCarDTO } from "../dtos/ICreateCarDTO";
 import { Car } from "../infra/typeorm/entities/Car";
 
 interface ICarsRepository {
-  cars: Car[] = [];
-
-  create(data: ICreateCarDTO): Promise<void>;
+  create(data: ICreateCarDTO): Promise<Car>;
+  findByLicensePlate(license_plate: string): Promise<Car>;
 }
 
 export { ICarsRepository };
