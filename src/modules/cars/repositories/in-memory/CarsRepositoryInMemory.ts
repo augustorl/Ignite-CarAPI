a811df54f3b5f5ffe01203cc4ad1b88797e3b930
augustorl <gutin22@gmail.com>
2021-04-06 00:27:23 -0300

    add: CarsRepositoryInMemory

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,32 @@
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
+import { ICarsRepository } from "../ICarsRepository";
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDTO): Promise<void> {
+    const cars = new Car();
+
+    Object.assign(cars, {
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+  }
+}
+
+export { CarsRepositoryInMemory };
