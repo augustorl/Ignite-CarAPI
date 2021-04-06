6909c2f423feef7c08a903a0af887294b31c6355
augustorl <gutin22@gmail.com>
2021-04-06 20:03:18 -0300

    Add: middlewares to routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 
+import { ensureUserIsAdmin } from "../middlewares/ensureUserIsAdmin";
+import { ensureUserIsAuthenticated } from "../middlewares/ensureUserIsAuthenticated";
+
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post(
+  "/",
+  ensureUserIsAuthenticated,
+  ensureUserIsAdmin,
+  createCarController.handle
+);
 
 export { carsRoutes };
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureUserIsAdmin } from "@shared/infra/http/middlewares/ensureUserIsAdmin";
 import { ensureUserIsAuthenticated } from "@shared/infra/http/middlewares/ensureUserIsAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureUserIsAuthenticated);
-specificationsRoutes.post("/", createSpecificationController.handle);
+specificationsRoutes.post(
+  "/",
+  ensureUserIsAuthenticated,
+  ensureUserIsAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationsRoutes };
