4a632c415e8df30d67381d89fada000df77df0b5
augustorl <gutin22@gmail.com>
2021-04-06 19:10:36 -0300

    fix: LIst Categories

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -6,7 +6,7 @@ import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 class ListCategoriesController {
   async handle(request: Request, response: Response): Promise<Response> {
     const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
-    const allCategories = listCategoriesUseCase.execute();
+    const allCategories = await listCategoriesUseCase.execute();
 
     return response.json(allCategories);
   }
