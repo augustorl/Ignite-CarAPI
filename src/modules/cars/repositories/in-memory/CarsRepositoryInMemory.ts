6f1268110ac6f57ede836c36c8c29c2c86fcf74e
augustorl <gutin22@gmail.com>
2021-04-07 00:30:23 -0300

    add: specifications and id to CarsRepositoryInMemory

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -14,7 +14,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
     fine_amount,
     brand,
     category_id,
-    speecifications,
+    id,
   }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
@@ -26,7 +26,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
       fine_amount,
       brand,
       category_id,
-      speecifications,
+      id,
     });
 
     this.cars.push(car);
