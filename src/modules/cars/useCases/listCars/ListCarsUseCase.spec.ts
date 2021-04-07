469f0b8a73b099c7e69394d226a3f6d6dc9c9c67
augustorl <gutin22@gmail.com>
2021-04-06 21:13:08 -0300

    Update: ListCarUseCase & test

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -12,7 +12,36 @@ describe("List Cars", () => {
   });
 
   it("should be able to list all available cars", async () => {
-    const cars = await listCarsUseCase.execute;
-    console.log(cars);
+    const car = await carsRepositoryInMemory.create({
+      name: "Test Car",
+      description: "Test car description",
+      daily_rate: 13.0,
+      license_plate: "ABC-1234",
+      fine_amount: 100,
+      brand: "test_brand",
+      category_id: "category_id",
+    });
+
+    const cars = await listCarsUseCase.execute({});
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Test Car",
+      description: "Test car description",
+      daily_rate: 13.0,
+      license_plate: "ABC-1234",
+      fine_amount: 100,
+      brand: "car_test_brand",
+      category_id: "category_id",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      brand: "car_test_brand",
+    });
+
+    expect(cars).toEqual([car]);
   });
 });
--- src/modules/cars/useCases/listCars/ListCarsUseCase.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.ts
@@ -3,15 +3,21 @@ import { inject, injectable } from "tsyringe";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
-@injectable()
-class ListCarsUseCase {
-  constructor(
-    @inject("CarsRepository")
-    private carsRepository: ICarsRepository
-  ) {}
+interface IRequest {
+  category_id?: string;
+  brand?: string;
+  name?: string;
+}
 
-  async execute(): Promise<Car[]> {
-    const cars = await this.carsRepository.findAllAvailableCars();
+class ListCarsUseCase {
+  constructor(private carsRepository: ICarsRepository) {}
+
+  async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
+    const cars = await this.carsRepository.findAllAvailableCars(
+      brand,
+      category_id,
+      name
+    );
 
     return cars;
   }
