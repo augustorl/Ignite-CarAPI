6baa73cb342871c026579dba0fa527a28cd4e482
augustorl <gutin22@gmail.com>
2021-04-12 12:39:33 -0300

    add: CreateRentalUseCase & test

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,16 +1,46 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
+
 interface IRequest {
   user_id: string;
   car_id: string;
-  expected_return_data: Date;
+  expected_return_date: Date;
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository);
+  constructor(private rentalsRepository: IRentalsRepository) {}
   async execute({
     user_id,
     car_id,
-    expected_return_data,
-  }: IRequest): Promise<void> {}
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavaible at this time!");
+    }
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError(
+        "There is a already a rental in progress for this user"
+      );
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
 }
 
 export { CreateRentalUseCase };
