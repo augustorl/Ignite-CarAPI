2368044e04e735b8f748c1d4f2bc99e97846203d
augustorl <gutin22@gmail.com>
2021-04-12 20:31:53 -0300

    Refactor: app to server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,5 @@
-import express, { NextFunction, Response, Request } from "express";
-import "reflect-metadata";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
+import { app } from "./app";
 
-import { AppError } from "@errors/AppError";
-import createConnection from "@shared/infra/typeorm/";
+app.listen(3333, () => console.log("Serv Running on port:3333"));
 
-import swaggerFile from "../../../swagger.json";
-import { router } from "./routes";
-
-import "@shared/container";
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(router);
-
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message}`,
-    });
-  }
-);
-app.listen(3333, () => console.log("Server runing on Port:3333"));
+export { app };
