2d2de410f1c078e2116a0249dd2d09b743c82073
augustorl <gutin22@gmail.com>
2021-04-29 15:40:43 -0300

    add: & implement findUserByIdAndRefreshToken

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -26,4 +26,20 @@ export default class UsersTokensRepository implements IUsersTokensRepository {
 
     return userToken;
   }
+
+  async findUserByIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    const userTokens = await this.repository.findOne({
+      user_id,
+      refresh_token,
+    });
+
+    return userTokens;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
 }
