d011835773907baab627c5ca9b9c783209da52d4
augustorl <gutin22@gmail.com>
2021-04-29 17:44:47 -0300

    update: RefreshTokenController

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -1,5 +1,19 @@
 import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import RefreshTokenUseCase from "./RefreshTokenUseCase";
 
 export default class RefreshTokenController {
-  async handle(request: Request, response: Response): Promise<Response> {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const token =
+      request.body.token ||
+      request.headers["x-access-token"] ||
+      request.query.token;
+
+    const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
+
+    const refreshToken = await refreshTokenUseCase.execute(token);
+
+    return response.json(refreshToken);
+  }
 }
