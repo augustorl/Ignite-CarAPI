98e525b267c3266923b13b7275903a471ac74900
augustorl <gutin22@gmail.com>
2021-04-29 15:09:49 -0300

    add: auth Config file

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "6f24b9d4c41c1256ef2d3b839ecac595",
+  expires_in_token: "15m",
+  secret_refresh_token: "37205e675358f9bb8b9f6cea27157ac1",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
