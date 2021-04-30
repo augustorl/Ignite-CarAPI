ec410837dd9007ebca3394e155902cf17277d151
augustorl <gutin22@gmail.com>
2021-04-29 21:15:03 -0300

    add: password routes to index

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticaRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
+import { passwordRoutes } from "./password.routes";
 import { rentalsRoutes } from "./rentals.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -14,7 +15,7 @@ router.use("/categories", categoriesRoutes);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
 router.use("/rentals", rentalsRoutes);
-
+router.use("/password", passwordRoutes);
 router.use(authenticaRoutes);
 
 export { router };
