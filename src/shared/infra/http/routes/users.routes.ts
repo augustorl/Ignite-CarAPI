2d8f8c58ab853687a8b0c561016cc3056bff631f
augustorl <gutin22@gmail.com>
2021-05-03 23:50:48 -0300

    add: ProfileUser to user routes

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,12 +3,14 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import ProfileUserController from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureUserIsAuthenticated } from "@shared/infra/http/middlewares/ensureUserIsAuthenticated";
 
 const usersRoutes = Router();
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
 
@@ -19,5 +21,11 @@ usersRoutes.patch(
   ensureUserIsAuthenticated,
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
+);
+
+usersRoutes.get(
+  "/profile",
+  ensureUserIsAuthenticated,
+  profileUserController.handle
 );
 export { usersRoutes };
