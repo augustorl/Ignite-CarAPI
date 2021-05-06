e86c76164cbcbb7b33bede7879282e9c568b7840
augustorl <gutin22@gmail.com>
2021-05-06 19:10:03 -0300

    add: Sentry and Tracing to app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,8 @@ import swaggerUi from "swagger-ui-express";
 
 import upload from "@config/upload";
 import { AppError } from "@errors/AppError";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm/";
 
@@ -19,15 +21,29 @@ createConnection();
 const app = express();
 
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
-
 app.use(router);
 
+app.use(Sentry.Handlers.errorHandler());
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: Error, _request: Request, response: Response, _next: NextFunction) => {
